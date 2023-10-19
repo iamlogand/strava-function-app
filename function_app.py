0e5d5c2e2d1a6c947e956f8d24811f295e1ef6fe
Logan Davidson <logan.davidson@autorek.com>
2023-10-19 08:44:03 +0100

    Make function execute once per hour

--- function_app.py
+++ function_app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
+@app.schedule(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
